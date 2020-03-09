f199082d5092af52d2e48bcc8ad3842d17725af8
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2020-03-09 12:19:10 +0100

    :alembic: Try overfit on the same easy level

--- procgen_adventure/entrypoint.py
+++ procgen_adventure/entrypoint.py
@@ -7,25 +7,43 @@ from rlpyt.algos.dqn.dqn import DQN
 from rlpyt.samplers.serial.sampler import SerialSampler
 
 
-def build_and_train(game="procgen:procgen-coinrun-v0", run_ID=0, cuda_idx=0):
-    sampler = SerialSampler(
-        EnvCls=gym_make,
-        env_kwargs=dict(
+def build_and_train(game="procgen:procgen-coinrun-v0"):
+    run_ID = 0
+    cuda_idx = 0
+    n_parallel = 4
+    config = dict(
+        env=dict(
             id=game,
             num_levels=1,
             start_level=32,
             distribution_mode="easy",
             paint_vel_info=True,
         ),
+        sampler=dict(batch_T=1, batch_B=16),
+        algo=dict(
+            min_steps_learn=int(2e4),
+            replay_size=int(1e5),
+            target_update_interval=int(8e3),
+            n_step_return=3,
+            learning_rate=0.0025,
+            eps_steps=int(25e4),
+            optim_kwargs=dict(eps=0.00015, weight_decay=0.0),
+            double_dqn=True,
+            prioritized_replay=True,
+        ),
+        affinity=dict(cuda_idx=cuda_idx, workers_cpus=list(range(n_parallel))),
+    )
+    sampler = SerialSampler(
+        EnvCls=gym_make,
+        env_kwargs=config["env"],
         eval_env_kwargs=dict(id=game),
-        batch_T=1,  # One time-step per sampler iteration.
-        batch_B=1,  # One environment (i.e. sampler Batch dimension)
         max_decorrelation_steps=0,
         eval_n_envs=10,
         eval_max_steps=int(10e3),
         eval_max_trajectories=5,
+        **config["sampler"]
     )
-    algo = DQN(double_dqn=True, prioritized_replay=True)  # Run with defaults.
+    algo = DQN(**config["algo"])  # Run with defaults.
     agent = ProcgenDqnAgent()
     runner = MinibatchRlEval(
         algo=algo,
@@ -33,13 +51,12 @@ def build_and_train(game="procgen:procgen-coinrun-v0", run_ID=0, cuda_idx=0):
         sampler=sampler,
         n_steps=50e6,
         log_interval_steps=1e3,
-        affinity=dict(cuda_idx=cuda_idx),
+        affinity=config["affinity"],
     )
     name = "dqn_" + "coinrun"
 
-    # with logger_context(run_ID, name, snapshot_mode="last"):
-    #    runner.train()
-    runner.train()
+    with logger_context(run_ID, name, snapshot_mode="last"):
+        runner.train()
 
 
 build_and_train()
