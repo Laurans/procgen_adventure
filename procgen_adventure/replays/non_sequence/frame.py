17cad8cd42d8bc10de0159d0d20750969731dd68
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2020-03-14 17:07:31 +0100

    :sparkles: Add replays buffer from rlpyt

---
+++ procgen_adventure/replays/non_sequence/frame.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+from procgen_adventure.replays.non_sequence.n_step import NStepReturnBuffer
+from procgen_adventure.replays.frame import FrameBufferMixin
+from procgen_adventure.replays.non_sequence.uniform import UniformReplay
+from procgen_adventure.replays.non_sequence.prioritized import PrioritizedReplay
+from procgen_adventure.replays.async_ import AsyncReplayBufferMixin
+
+
+class NStepFrameBuffer(FrameBufferMixin, NStepReturnBuffer):
+    """Special method for re-assembling observations from frames."""
+
+    def extract_observation(self, T_idxs, B_idxs):
+        """Assembles multi-frame observations from frame-wise buffer.  Frames
+        are ordered OLDEST to NEWEST along C dim: [B,C,H,W].  Where
+        ``done=True`` is found, the history is not full due to recent
+        environment reset, so these frames are zero-ed.
+        """
+        # Begin/end frames duplicated in samples_frames so no wrapping here.
+        # return np.stack([self.samples_frames[t:t + self.n_frames, b]
+        #     for t, b in zip(T_idxs, B_idxs)], axis=0)  # [B,C,H,W]
+        observation = np.stack(
+            [
+                self.samples_frames[t : t + self.n_frames, b]
+                for t, b in zip(T_idxs, B_idxs)
+            ],
+            axis=0,
+        )  # [B,C,H,W]
+        # Populate empty (zero) frames after environment done.
+        for f in range(1, self.n_frames):
+            # e.g. if done 1 step prior, all but newest frame go blank.
+            b_blanks = np.where(self.samples.done[T_idxs - f, B_idxs])[0]
+            observation[b_blanks, : self.n_frames - f] = 0
+        return observation
+
+
+class UniformReplayFrameBuffer(UniformReplay, NStepFrameBuffer):
+    pass
+
+
+class PrioritizedReplayFrameBuffer(PrioritizedReplay, NStepFrameBuffer):
+    pass
+
+
+class AsyncUniformReplayFrameBuffer(AsyncReplayBufferMixin, UniformReplayFrameBuffer):
+    pass
+
+
+class AsyncPrioritizedReplayFrameBuffer(
+    AsyncReplayBufferMixin, PrioritizedReplayFrameBuffer
+):
+    pass
